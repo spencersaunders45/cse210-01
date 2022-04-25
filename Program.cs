3cb8d606347b37396234f799cd1b8917167042c1
Spencer Saunders <spencer.saunders45@gmail.com>
2022-04-25 12:44:53 -0600

    creating game loop

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+    Spencer Saunders
+    CSE 210 - Tic Tak Toe
+*/
+
+using System;
+
+namespace cse210_01{
+    class Program{
+        static void Main(string[] args)
+        {
+            bool gameOver = false;
+            string gameBoard = "1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9";
+
+            while (gameOver == false){
+                
+            }
+            
+            displayBoard(gameBoard);
+        }
+
+        static void displayBoard(string gameBoard){
+        for (int i = 0; i < gameBoard.Length; i++){
+                Console.Write(gameBoard[i]);
+            }
+        }
+    }
+}
