570985d611ae49e01154ae8917816616a9a0c767
Spencer Saunders <spencer.saunders45@gmail.com>
2022-04-26 20:29:28 -0600

    players can make moves on board

--- Program.cs
+++ Program.cs
@@ -10,18 +10,26 @@ namespace cse210_01{
         static void Main(string[] args)
         {
             bool gameOver = false;
-            string gameBoard = "123456789";
+            char[] gameBoard = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
             string winner = " ";
 
             while (gameOver == false){
                 displayBoard(gameBoard);
-                // int x = Convert.ToInt32(Console.ReadLine());
-                gameOver = true;
+
+                Console.Write("x\'s turn to choose a square(1-9): ");
+                int xMove = Convert.ToInt32(Console.ReadLine()) - 1;
+                gameBoard = addMove(gameBoard, xMove, 'x', ref winner);
+                
+                displayBoard(gameBoard);
+
+                Console.Write("o\'s turn to choose a square(1-9): ");
+                int oMove = Convert.ToInt32(Console.ReadLine()) - 1;
+                gameBoard = addMove(gameBoard, oMove, 'o', ref winner);
             }
             
         }
 
-        static void displayBoard(string gameBoard){
+        static void displayBoard(char[] gameBoard){
             for (int i = 0; i < gameBoard.Length; i++){
                 if (i % 3 == 0 && i > 0){
                     Console.WriteLine("\n-+-+-");
@@ -35,6 +43,20 @@ namespace cse210_01{
                 }
                 Console.Write("|");
             }
+            Console.Write("\n\n");
         }
+
+        static char[] addMove(char[] gameBoard, int move, char player, ref string winner){
+            if (!gameBoard[move].Equals("x") || !gameBoard[move].Equals("o")){
+                if (player.Equals('o')){
+                    gameBoard[move] = 'o';
+                } else if (player.Equals('x')){
+                    gameBoard[move] = 'x';
+                }
+            }
+
+            return gameBoard;
+        }
+
     }
 }
